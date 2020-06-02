9f9199f8e4984bd535970798c13a981eb90d882d
RaphaelOliveiraMoura <raphaeldeoliveiramoura@gmail.com>
2020-06-02 17:39:45 -0300

    feat: pre-rendering and data fetching

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,21 @@
 import Head from "next/head";
+
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 
-export default function Home() {
+import { getSortedPostsData } from "../lib/posts";
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -14,6 +27,20 @@ export default function Home() {
           Js Enthusiast and improving development code skills with contruibuting
           in open source projects
         </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
